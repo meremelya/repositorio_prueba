0a01a2613ec2875c4963bb2dee29167565ffce81
Melkiy <Melkiy@Melkiy-PC>
2019-05-25 23:41:09 +0200

    Version 1.0 version base

---
+++ prueba_repositorio/src/prueba_repositorio/clase_prueba.java
@@ -0,0 +1,11 @@
+package prueba_repositorio;
+
+public class clase_prueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola");
+
+	}
+
+}
